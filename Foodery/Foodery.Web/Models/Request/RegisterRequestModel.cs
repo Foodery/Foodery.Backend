30c8976d05db03c7e3fb693d55cf123c29f9aa9e
belchevgb <gabriel.belchev@protonmail.com>
2017-11-18 13:12:51 +0200

    configuring CI

---
+++ Foodery/Foodery.Web/Models/Request/RegisterRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Foodery.Web.Models.Request
+{
+    public class RegisterRequestModel
+    {
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Foodery/Tests/Unit/Foodery.Unit.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Foodery.Unit.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void AssertTrueIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
